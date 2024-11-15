436831357b18af943d751b10a42f810dad605ad4
potados99 <potados99@gmail.com>
2024-11-15 21:12:23 +0900

    chore: 찌불 캡챠뚫기 실패

--- src/scenarios/YdBB2024.ts
+++ src/scenarios/YdBB2024.ts
@@ -1,4 +1,5 @@
 import {Scenario} from '../../lib/common/TicketingSite';
+import Tesseract from 'tesseract.js';
 
 export function ydbb2024(params: {date: Date, block: string, delay: number}): Scenario {
   const goodsCode = `24016375`;
@@ -41,9 +42,31 @@ export function ydbb2024(params: {date: Date, block: string, delay: number}): Sc
             execute: async (page) => {
               const s = `#productSide > div > div.sideBtnWrap > a.sideBtn.is-primary`;
               await page.waitForSelector(s, {visible: true});
-              await page.$eval(s, e => e.click());
+              await page.click(s); // 진짜 클릭을 유발시켜야 클릭 이벤트의 isTrusted가 true가 됩니다. 그래야 헤드리스 브라우저 탐지를 속일 수 있음 ㅋㅎㅋㅎ
 
               return await new Promise(x => page.once('popup', x));
+            }
+          }
+        ]
+      },
+      {
+        name: '캡챠 회피기동',
+        instructions: [
+          {
+            name: '코드 읽고 입력',
+            execute: async (page) => {
+              const frameSelector = 'iframe[id="ifrmSeat"]';
+              await page.waitForSelector(frameSelector, {visible: true});
+
+              const iframe = await page.$(frameSelector);
+              const contentFrame = await iframe!.contentFrame();
+
+              const imgSelector = `#imgCaptcha`;
+              await contentFrame.waitForSelector(imgSelector, {visible: true});
+              const src = await contentFrame.$eval(imgSelector, e => (e as HTMLImageElement).src);
+              const result = await Tesseract.recognize(src, 'eng', {logger: console.log});
+
+              console.log(result.data.text);
             }
           }
         ]
